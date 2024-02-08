59ccb416c900840d4aa3741c84738467bd982f63
taboubi <taboubioussema78@gmail.com>
2024-02-08 13:44:43 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, TextInput, StatusBar } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [cal, setCal] = useState("");
+
+  const num = Number(cal) * 44187.9;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#000",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 40,
+      }}
+    >
+      <StatusBar hidden  />
+
+      <Text style={[styles.text, { fontSize: 36 }]}>Hi,</Text>
+      <Text style={[styles.text, { fontSize: 16 }]}>
+        Wellcome to simple bitcoin calculator
+      </Text>
+      <View style={{ flexDirection: "row" }}>
+        <TextInput
+          keyboardType="decimal-pad"
+          style={[styles.border, styles.money]}
+          onChangeText={(val) => setCal(val)}
+        />
+        <View style={styles.border2}>
+          <Text style={styles.money}>BTC</Text>
+        </View>
+      </View>
+      <View style={{ flexDirection: "row" }}>
+        <Text
+          style={[
+            styles.border,
+            styles.money,
+            { padding: 25, fontSize: Number(cal) < 19990 ? 20 : 10 },
+          ]}
+        >
+          {num}
+        </Text>
+        <View style={styles.border2}>
+          <Text style={styles.money}>USD</Text>
+        </View>
+      </View>
+
+      {Number(cal) > 100000 ? (
+        <Text style={[styles.money, { textAlign: "center" }]}>
+          What ! you have {cal} bitcoin{"\n"}(min ayna laka hadha?){" "}
+        </Text>
+      ) : null}
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  text: {
+    fontWeight: "700",
+    fontStyle: "normal",
+    color: "#FFFFFFA6",
+  },
+  money: {
+    fontSize: 20,
+    fontWeight: "700",
+    fontStyle: "normal",
+
+    color: "#FFFFFFA6",
+  },
+  border: {
+    backgroundColor: "#313949",
+    height: 80,
+    width: 200,
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    padding: 15,
+  },
+  border2: {
+    backgroundColor: "#313959",
+    height: 80,
+    width: 100,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
